b1ccf3a38ecc3f18f06cd079f58bce90866ea030
JIvy42 <Jeremiahjivy@gmail.com>
2025-03-25 02:22:16 +0000

    Labs (1)

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Name: Jeremiah");
+        System.out.println("Favorite Food: Wings");
+        System.out.println("Celebrity I'd like to meet: Travis Scott");
+        System.out.println("Least Favorite Chore: Doing the dishes");
+        System.out.println("Playing video games");
+    }
+}
+/*
+Block Comments:
+1. What does the main() method represent in a Java program?
+   - The main() method serves as the entry point of a Java program.
+2. Why is System.out.println() used to display output?
+   - System.out.println() is a Java method used to output text to the console. It prints the provided message or data.
+   3. What is the purpose of comments in the program?
+   - Comments provide explanations or notes about the code to make it easier to understand for anyone reading it. 
+*/
